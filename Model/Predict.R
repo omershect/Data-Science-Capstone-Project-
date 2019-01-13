4e087f731b30de332d608d0b86dfe37aa9a91d6d
unknown <omershect@hotmail.com>
2019-01-13 21:58:46 +0200

    Benchmark achive good first results

--- Model/Predict.R
+++ Model/Predict.R
@@ -26,22 +26,32 @@ setkey(four_words, word_1, word_2, word_3,word_4)
 five_words<-data.table(five_words)
 setkey(five_words, word_1, word_2, word_3,word_4,word_5)
 
-#Function to predict the next word based on a string 
-Predict_Simple_Back_of <- function(str,Ngram = 5){
+
+#########################################################
+#                                                       #
+#Function to predict the next word based on a string    #
+
+#########################################################
+
+
+Predict_Simple_Back_of <- function(str,NoOfWordtoReturn =5){
   
   
   uni_words<-data.table(uni_words)
   setkey(uni_words, word_1)
   
-  
+  str<-iconv(str, from = "UTF-8", to = "ASCII", sub = "")
+  #Calculate the length of the sentence
   n<-sapply(strsplit(str," "), length)
    
+  #Convert the sentence into tokens
   tokens <- tokens(x = char_tolower(str),remove_punct = TRUE,
-      remove_twitter = TRUE,
+    remove_twitter = TRUE,
     remove_numbers = TRUE,
     removeNumbers  = TRUE,
     remove_hyphens = TRUE,
     remove_symbols = TRUE,
+    remove_separators = TRUE,
     remove_url = TRUE)
   
   word1<-""
@@ -51,31 +61,41 @@ Predict_Simple_Back_of <- function(str,Ngram = 5){
   
   # Split to words according to the numbe of words 
   if (n>=4 ) {
-    word1<-tokens$text1[n-3]
-    word2<-tokens$text1[n-2]
-    word3<-tokens$text1[n-1]
-    word4<-tokens$text1[n]
+      word1<-tokens$text1[n-3]
+      word2<-tokens$text1[n-2]
+      word3<-tokens$text1[n-1]
+      word4<-tokens$text1[n]
   }
   
   if (n>=3 ) {
-    word2<-tokens$text1[n-2]
-    word3<-tokens$text1[n-1]
-    word4<-tokens$text1[n]
+      word2<-tokens$text1[n-2]
+      word3<-tokens$text1[n-1]
+      word4<-tokens$text1[n]
   }
   
   if (n>=2 ) {
-    word3<-tokens$text1[n-1]
-    word4<-tokens$text1[n]
+      word3<-tokens$text1[n-1]
+      word4<-tokens$text1[n]
   }
   
   if (n>=1 ) 
     word4<-tokens$text1[n]
+  if (n==0 )
+    word4<-" "
   
-#Search 5 Grams 
-#And calculate the probabilty  
-
+  #Initiliaze the length parameters 
+  PredictLength<-0
+  Words5_length<-0
+  Words4_length<-0
+  Words3_length<-0
+  Words2_length<-0
+  
+  #Search 5 Grams 
+  #And calculate the probabilty 
   Five_Gram_Search<-five_words[.(word1,word2,word3,word4)]
+  #If The last word is NA - No match - Remove the entire row
   Five_Gram_Search_NoNA<-na.omit (Five_Gram_Search, "count") 
+  #If there is at least one occurrence calculate the probability 
   if (dim(Five_Gram_Search_NoNA)[1]>0){
        
         Count4Grams<-four_words[.(word1,word2,word3,word4)]$count
@@ -84,16 +104,15 @@ Predict_Simple_Back_of <- function(str,Ngram = 5){
         Words5<-select(Words5,word_5,Prob)
         names(Words5)<-c("word","Prob")
         Words5_length<-dim(Words5)[1]
-    } else {
-      Words5_length<-0
-       
-     }
-  
-  
-#Four_Gram_Search<- subset(four_words,word_1==word2 & word_2==word3 & word_3==word4)
-Four_Gram_Search<-four_words[.(word2,word3,word4)]
-Four_Gram_Search_NoNA<-na.omit (Four_Gram_Search, "count") 
-if (dim(Four_Gram_Search_NoNA)[1]>0){
+  } 
+#update the length   
+PredictLength<-PredictLength+Words5_length
+
+if (PredictLength<NoOfWordtoReturn) {  
+  #Four_Gram_Search<- subset(four_words,word_1==word2 & word_2==word3 & word_3==word4)
+  Four_Gram_Search<-four_words[.(word2,word3,word4)]
+  Four_Gram_Search_NoNA<-na.omit (Four_Gram_Search, "count") 
+  if (dim(Four_Gram_Search_NoNA)[1]>0){
       #Count3Grams<-subset(tri_words, word_1==word2 & word_2==word3 & word_3==word4)$count 
       Count3Grams<-tri_words[.(word2,word3,word4)]$count 
       Words4<-select(Four_Gram_Search,word_4,count)
@@ -101,15 +120,20 @@ if (dim(Four_Gram_Search_NoNA)[1]>0){
       Words4<-select(Words4,word_4,Prob)
       names(Words4)<-c("word","Prob")
       Words4_length<-dim(Words4)[1]
-} else {
-  Words4_length<-0
+  } else {
+    Words4_length<-0
   
+  }
 }
+ 
+PredictLength<-PredictLength+Words4_length
   
-#Tree_Gram_Search<- subset(tri_words,word_1==word3 & word_2==word4)
-Tree_Gram_Search<-tri_words[.(word3,word4)]
-Tree_Gram_Search_NoNA<-na.omit (Tree_Gram_Search, "count") 
-if (dim(Tree_Gram_Search_NoNA)[1]>0){
+if (PredictLength<NoOfWordtoReturn) {  
+   
+  #Tree_Gram_Search<- subset(tri_words,word_1==word3 & word_2==word4)
+  Tree_Gram_Search<-tri_words[.(word3,word4)]
+  Tree_Gram_Search_NoNA<-na.omit (Tree_Gram_Search, "count") 
+  if (dim(Tree_Gram_Search_NoNA)[1]>0){
       #Count2Grams<-subset(bi_words,word_1==word3 & word_2==word4)$count 
       Count2Grams<-bi_words[.(word3,word4)]$count
       Words3<-select(Tree_Gram_Search,word_3,count)
@@ -117,13 +141,15 @@ if (dim(Tree_Gram_Search_NoNA)[1]>0){
       Words3<-select(Words3,word_3,Prob)
       names(Words3)<-c("word","Prob")
       Words3_length<-dim(Words3)[1]
-} else {
-  Words3_length<-0
+  } else {
+    Words3_length<-0
   
+  }
 }
   
-  
-#Bi_Gram_Search<- subset(bi_words,word_1==word4)
+PredictLength<-PredictLength+Words3_length  
+if (PredictLength<NoOfWordtoReturn) {  
+  #Bi_Gram_Search<- subset(bi_words,word_1==word4)
   Bi_Gram_Search<-bi_words[.(word4)]
   Bi_Gram_Search_NoNA<-na.omit (Bi_Gram_Search, "count") 
   if (dim(Bi_Gram_Search_NoNA)[1]>0){
@@ -138,8 +164,17 @@ if (dim(Tree_Gram_Search_NoNA)[1]>0){
     Words2_length<-0
     
   }
+  
+}
+
+
+
+
+
+
+
   Pwords<-""
-  PredictLength<-Words5_length+Words4_length+Words4_length+Words4_length
+  
   if(Words5_length>0)
     Pwords<-Words5
   if(Words4_length>0)
@@ -154,9 +189,9 @@ if (dim(Tree_Gram_Search_NoNA)[1]>0){
   
   #in case the search find less then 5 words - complete with words with highest 
   # probabilty from Uni gram
-  if(PredictLength < 5) {
+  if(PredictLength < NoOfWordtoReturn) {
     uni<-uni_words
-    UniWordsHigh<-select(head(setorder(uni,colS=-Prob),5 ),word_1,Prob)
+    UniWordsHigh<-select(head(setorder(uni,colS=-Prob),5 ),word_1,count)
     names(UniWordsHigh)<-c("word","Prob")
     if (PredictLength == 0)
       Pwords<-UniWordsHigh
@@ -178,9 +213,10 @@ if (dim(Tree_Gram_Search_NoNA)[1]>0){
 
 
 
-Predict_Words <- function(str,Ngram=5){
+Predict_Words <- function(str){
   
-Words<-Predict_Simple_Back_of(str,Ngram)
+
+Words<-Predict_Simple_Back_of(str)
 res<-select(Words,word)[1:3,]
 return ((res))
 
